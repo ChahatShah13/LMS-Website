f788bdb2498d858e79fd99480511e67a4ce392a3
ChahatShah13 <chahatshah2002@gmail.com>
2025-03-08 00:08:29 +0530

    Course Details Page Right Section Is Created

--- client/src/components/students/Loading.jsx
+++ client/src/components/students/Loading.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import React from "react";
+import { Loader2 } from "lucide-react";
 
 const Loading = () => {
   return (
-    <div>
-      <h1>Loading</h1>
+    <div className="flex items-center justify-center h-screen bg-gradient-to-b from-cyan-100/70">
+      <div className="flex flex-col items-center p-6 bg-white rounded-2xl shadow-lg">
+        <Loader2 className="w-16 h-16 animate-spin text-blue-500" />
+        <h1 className="mt-4 text-xl font-bold text-gray-700">Please Wait...</h1>
+        <p className="text-sm text-gray-500">Fetching data for you</p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Loading
+export default Loading;
--- client/src/pages/students/CourseDetails.jsx
+++ client/src/pages/students/CourseDetails.jsx
@@ -4,10 +4,12 @@ import { AppContext } from "../../context/AppContext";
 import Loading from "../../components/students/Loading";
 import { assets } from "../../assets/assets";
 import humanizeDuration from "humanize-duration";
+import Footer from "../../components/students/Footer";
 const CourseDetails = () => {
   const { id } = useParams();
   const [courseData, setCourseData] = useState(null);
   const [openSection, setOpenSection] = useState({});
+  const [isAlreadyEnrolled, setIsAlreadyEnrolled] = useState(false);
 
   const {
     allCourses,
@@ -15,6 +17,7 @@ const CourseDetails = () => {
     calculateTotalCourseDuration,
     calculateChapterTime,
     calculateTotalLectures,
+    currency,
   } = useContext(AppContext);
   const fetchCourseData = async () => {
     const findCourse = allCourses.find((course) => course._id === id);
@@ -149,7 +152,9 @@ const CourseDetails = () => {
           </div>
 
           <div className="py-20 text-sm md:text-default">
-            <h3 className="text-xl font-semibold text-gray-800">Course Description</h3>
+            <h3 className="text-xl font-semibold text-gray-800">
+              Course Description
+            </h3>
             <p
               className="pt-3 rich-text"
               dangerouslySetInnerHTML={{
@@ -160,8 +165,81 @@ const CourseDetails = () => {
         </div>
 
         {/* Right Column */}
-        <div></div>
+        <div className="max-w-cursor-card z-10 shadow-custom-card rounded-t md:rounded-none overflow-hidden bg-white min-w-[300px] sm:min-w-[420px]">
+          <img src={courseData.courseThumbnail} alt="Course Thumbnail" />
+          <div className="p-5">
+            <div className="flex items-center gap-2">
+              <img
+                className="w-3.5"
+                src={assets.time_left_clock_icon}
+                alt="time left clock icon"
+              />
+              <p className="text-red-500">
+                {" "}
+                <span className="font-medium"> 5 days </span> left at this
+                price!
+              </p>
+            </div>
+            <div className="flex items-center gap-3 pt-2">
+              <p className="text-gray-800 md:text-4xl text-2xl font-semibold">
+                {currency}{" "}
+                {(
+                  courseData.coursePrice -
+                  (courseData.discount * courseData.coursePrice) / 100
+                ).toFixed(2)}
+              </p>
+              <p className="md:text-lg text-gray-500 line-through">
+                {currency} {courseData.coursePrice}
+              </p>
+              <p className="md:text-lg text-gray-500 ">
+                {courseData.discount}% off
+              </p>
+            </div>
+            <div className="flex items-center text-sm md:text-default gap-4 pt-2 md-pt-4 text-gray-500 ">
+              <div className="flex items-center gap-1">
+                <img src={assets.star} alt="star" />
+                <p>{calculateRating(courseData)}</p>
+              </div>
+
+              <div className="h-4 w-px bg-gray-500/40"></div>
+
+              <div className="flex items-center gap-1">
+                <img src={assets.time_clock_icon} alt="time_clock_icon" />
+                <p>{calculateTotalCourseDuration(courseData)}</p>
+              </div>
+
+              <div className="h-4 w-px bg-gray-500/40"></div>
+
+              <div className="flex items-center gap-1">
+                <img src={assets.lesson_icon} alt="" />
+                <p>{calculateTotalLectures(courseData)} Lessons</p>
+              </div>
+            </div>
+            <button
+              className={`md:mt-6 mt-4  w-full py-3 rounded text-white font-medium ${
+                isAlreadyEnrolled
+                  ? "bg-gray-400 cursor-not-allowed"
+                  : "bg-blue-600 cursor-pointer"
+              }`}
+              disabled={isAlreadyEnrolled}
+            >
+              {isAlreadyEnrolled ? "Already Enrolled" : "Enroll Now"}
+            </button>
+
+            <div className="pt-6">
+              <p className="md:text-xl text-lg font-medium text-gray-800">What's in the course?</p>
+              <ul className="ml-4 pt-2 text-sm md:text-default list-disc text-gray-500">
+                <li>Lifetime access with free updates.</li>
+                <li>Step-by-step, hands-on project guidance.</li>
+                <li>Downloadable resources and source code.</li>
+                <li>Quizzes to test your knowledge.</li>
+                <li>Certificate of completion.</li>
+              </ul>
+            </div>
+          </div>
+        </div>
       </div>
+      <Footer/>
     </>
   ) : (
     <Loading />
