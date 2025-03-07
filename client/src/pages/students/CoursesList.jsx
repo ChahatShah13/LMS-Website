bf4a10b9264e69414bed679ecc9ea79187c22e1a
ChahatShah13 <chahatshah2002@gmail.com>
2025-03-07 11:13:17 +0530

    Filter Course Based On Search Input, Footer Added

--- client/src/pages/students/CoursesList.jsx
+++ client/src/pages/students/CoursesList.jsx
@@ -1,37 +1,71 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { AppContext } from "../../context/AppContext";
 import SearchBar from "../../components/students/SearchBar";
 import { useParams } from "react-router-dom";
 import CourseCard from "../../components/students/CourseCard";
+import { assets } from "../../assets/assets";
+import Footer from "../../components/students/Footer";
 
 const CoursesList = () => {
-  const { navigate,allCourses } = useContext(AppContext);
+  const { navigate, allCourses } = useContext(AppContext);
   const { input } = useParams();
+  const [filteredCourses, setFilteredCourses] = useState([]);
+
+  useEffect(() => {
+    if (allCourses && allCourses.length > 0) {
+      const tempCourses = allCourses.slice();
+      input
+        ? setFilteredCourses(
+            tempCourses.filter((item) =>
+              item.courseTitle.toLowerCase().includes(input.toLowerCase())
+            )
+          )
+        : setFilteredCourses(tempCourses);
+    }
+  }, [allCourses, input]);
 
   return (
-    <div className="relative px-6 md:px-36 pt-10">
-      <div className="flex flex-col md:flex-row items-center justify-between w-full gap-6">
-        <div className="text-left">
-          <h1 className="text-4xl font-semibold text-gray-800">Course List</h1>
-          <p className="text-gray-500">
-            <span
-              className="text-blue-600 cursor-pointer hover:underline"
-              onClick={() => navigate("/")}
-            >
-              Home
-            </span>{" "}
-            / <span>Course List</span>
-          </p>
+    <>
+      <div className="relative px-6 md:px-36 pt-10">
+        <div className="flex flex-col md:flex-row items-center justify-between w-full gap-6">
+          <div className="text-left">
+            <h1 className="text-4xl font-semibold text-gray-800">
+              Course List
+            </h1>
+            <p className="text-gray-500">
+              <span
+                className="text-blue-600 cursor-pointer hover:underline"
+                onClick={() => navigate("/")}
+              >
+                Home
+              </span>{" "}
+              / <span>Course List</span>
+            </p>
+          </div>
+          {/* Search Bar aligned to the right */}
+          <div className="w-full md:w-auto">
+            <SearchBar data={input} />
+          </div>
         </div>
-        {/* Search Bar aligned to the right */}
-        <div className="w-full md:w-auto">
-          <SearchBar data={input} />
+        {input && (
+          <div className="inline-flex items-center gap-4 px-4 py-2 border mt-8-mb-8 text-gray-600">
+            <p>{input}</p>
+            <img
+              src={assets.cross_icon}
+              alt=""
+              className="cursor-pointer"
+              onClick={() => navigate("/course-list")}
+            />
+          </div>
+        )}
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3 px-2 md:p-0 my-16">
+          {filteredCourses.map((course, index) => (
+            <CourseCard key={index} course={course} />
+          ))}
         </div>
       </div>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3 px-2 md:p-0 my-16">
-        {allCourses.map((course,index)=><CourseCard key={index} course={course}/>)}
-      </div>
-    </div>
+      <Footer/>
+    </>
   );
 };
 
