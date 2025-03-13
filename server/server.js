efbfb23acf54e896977484faa9a705dc8b73a8c8
ChahatShah13 <chahatshah2002@gmail.com>
2025-03-13 12:42:28 +0530

    User Model and Clerk Webhook created

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
 import connectDB from './configs/mongodb.js'
+import { clerkWebhooks } from './controllers/webHooks.js'
 
 const app=express()
 
@@ -13,6 +14,7 @@ app.use(cors())
 
 // Routes 
 app.get('/',(req,res)=>res.send('Api Working'))
+app.post('/clerk',express.json(),clerkWebhooks)
 
 // Port No.
 
